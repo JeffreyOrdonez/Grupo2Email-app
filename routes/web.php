e3e17ac62ccd3351df9dddc65da2d7d6353a9835
Nilson Saballos A <nilson.saballos@tecnacional.edu.ni>
2025-05-27 20:02:24 -0600

    Cambios locales en web.php

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,13 @@ use App\Http\Controllers\CityController;
 use App\Http\Controllers\CitizenController;
 use App\Http\Controllers\ReportCitizenController;
 use App\Http\Controllers\viewgroupcontroller;
+use App\Http\Controllers\dashboardcontroller;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [dashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
